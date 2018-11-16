6d08800cac41631f80a0aff94123c1f77e9864ba
penlinx <benkariuki@gmail.com>
2018-11-17 02:11:37 +0300

    Initial commit

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,14 @@
-
+import math
+def primeNumbers(maxint):
+    '''
+    This function generates all prime numbers less than or equal to the supplied value 
+    '''
+    primes = []
+    for num in range(2, maxint + 1):
+        isPrime = True
+        for div in range(2, int(math.sqrt(num)) + 1):
+            if (num % div == 0):
+        	isPrime = False
+	if isPrime:
+	    primes.append(num)
+    return primes
